0f1b6cade4ee6dea1dbcc147d9c0aab98ab5be4a
Bhuvan Ganesan <bhuvan.ganesan@gmail.com>
2022-12-20 09:52:11 +0530

    update on typescript exp code

--- 01_Getting_Started/angular-app/src/app/app.component.ts
+++ 01_Getting_Started/angular-app/src/app/app.component.ts
@@ -7,6 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   inputField = 'angular-app';
+  num : Number = 0;
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    // this.num = 'safdsa'; 
+    console.log(this.num);
+  }
+
+ 
 
 
 }
