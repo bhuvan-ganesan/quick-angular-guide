ed8805b958351bf089658da802c1996c148a10f1
Bhuvan Ganesan <bhuvan.ganesan@gmail.com>
2022-12-22 08:08:30 +0530

    update on typescript

--- 01_Getting_Started/angular-app/src/app/app.component.ts
+++ 01_Getting_Started/angular-app/src/app/app.component.ts
@@ -1,20 +1,47 @@
 import { Component } from '@angular/core';
+import {Person} from './in';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   inputField = 'angular-app';
   num : Number = 0;
+  john = {
+    firstName: 'John',
+    lastName: ['Doe']
+  };
+
+  constructor(){
+
+  }
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
     // this.num = 'safdsa'; 
     console.log(this.num);
+
+   
+    
+    console.log(this.getFullName(this.john));
+
   }
+
+
+
+getFullName(person: Person) {
+  return `dsfsdf dsf dsfds`;
+  // return `${person.firstName} ${person.lastName}`;
+}
+
+
+
+
+
 
  
 
